d972000df5e8bf605a133fcf61c25a1abb7cdc1e
supriya <supriya1213@sasi.ac.in>
2023-04-03 20:49:36 +0530

    Task 14

---
+++ replace string.py
@@ -0,0 +1,13 @@
+str='I am learning Python There is alot of methods'
+print(str)
+k=str.replace('is','are')
+print(k)
+e=str.replace('There','That')
+print(e)
+
+
+
+output:
+I am learning Python There is alot of methods
+I am learning Python There are alot of methods
+I am learning Python That is alot of methods
