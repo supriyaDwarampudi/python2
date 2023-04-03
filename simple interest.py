6dfed2b934d97b930eb7ce3ebbca8c71b72cdff1
supriya <supriya1213@sasi.ac.in>
2023-04-03 20:35:55 +0530

    Task 5

---
+++ simple interest.py
@@ -0,0 +1,13 @@
+# find the simple interest
+P=int(input('Enter the principle value'))
+R=int(input('Enter the rate value'))
+T=int(input('Enter the time value'))
+A=P*(1+R*T)
+print(A)
+
+
+output
+Enter the principle value12
+Enter the rate value2
+Enter the time value14
+348
