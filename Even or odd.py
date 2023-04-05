9c34f10ab09daac4ab5f7d76246afe56ceae06c0
supriya <supriya1213@sasi.ac.in>
2023-04-05 11:18:06 +0530

    IMP INTERVIEW QUESTION 2

---
+++ Even or odd.py
@@ -0,0 +1,18 @@
+# check if the number is even or odd
+
+# if number is divisible by 2 and gives remainder as 0 then it consider to be as even number.
+
+# if number is not divisible by 2 and gives remainder as 1 then it consider to be as odd number.
+
+# CODE:
+
+n=float(input('Enter a number'))
+if n%2==0:
+    print('Given number is even number')
+else:
+    print('Given number is odd number')
+
+# OUTPUT:
+
+# Enter a number20.0
+# Given number is even number
