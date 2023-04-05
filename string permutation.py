10e348abdff932dade15cce02c314859b435d5ce
supriya <supriya1213@sasi.ac.in>
2023-04-05 12:39:15 +0530

    IMP INTERVIEW QUESTION 5

---
+++ string permutation.py
@@ -0,0 +1,8 @@
+def get_permutations(string,i=0):
+    if i==len(string):
+        print("".join(string))
+    for j in range(i,len(string)):
+        words=[c for c in string]
+        words[i],words[j]=words[j],words[i]
+        get_permutations(words,i+1)
+get_permutations('supriya')
