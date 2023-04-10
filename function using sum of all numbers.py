ab85023361041a90da3b8350bb224e168e949f3a
supriya <supriya1213@sasi.ac.in>
2023-04-10 21:00:43 +0530

    IMP INTERVIEW QUESTION 9

---
+++ function using sum of all numbers.py
@@ -0,0 +1,15 @@
+# def sum(numbers):
+#     total=0
+#     for x in numbers:
+#         total+=x
+#     return total
+# print(sum([13,14,17,15,28,29,31]))
+# output:
+# 147
+
+#    OR
+r=[13,14,15,17,28,29,31]
+print(sum(r)
+
+      # output:
+      #   147
